0aef555923e7e73d3a5582a54cec522e0242840a
edangolgithub <dangolevan@gmail.com>
2020-12-30 04:48:29 +0000

    xcf

--- nodedockers/react1/hello-world/src/App.js
+++ nodedockers/react1/hello-world/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import HelloWorld from './HelloWorld';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <HelloWorld />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
